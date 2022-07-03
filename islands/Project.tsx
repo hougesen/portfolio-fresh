c40258e032a4588a3fd45e39f0107584d0f2faf9
Mads Hougesen <madshougesen@gmail.com>
2022-07-03 13:35:30 +0200

    style: change white hex color

--- islands/Project.tsx
+++ islands/Project.tsx
@@ -5,7 +5,7 @@ import { IProject } from '../routes/index.tsx';
 
 export default function Project({ project }: { project: IProject }) {
     return (
-        <div class={tw`w-full bg-white p-4 flex flex-col border border-solid border-[#101010] min-h-[12rem]`}>
+        <div class={tw`w-full bg-[#f7f7f7] p-4 flex flex-col border border-solid border-[#101010] min-h-[12rem]`}>
             <div class={tw`flex`}>
                 <a href={project?.url ?? project?.homepageUrl} target='_blank' rel='noreferrer noopener'>
                     <h2 class={tw`text-xl font-semibold mb-1 lowercase text-[#101010]`}>{project.name}</h2>
