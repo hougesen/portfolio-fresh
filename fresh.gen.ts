b872dd38d7d4dd5890fd4988f043882a9d3115e7
hougesen <hougesen@users.noreply.github.com>
2022-07-06 20:32:24 +0000

    Formatting changes from formatter.yml

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,25 +2,25 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/blog/[...slug].tsx";
-import * as $1 from "./routes/index.tsx";
-import * as $$0 from "./islands/Hero.tsx";
-import * as $$1 from "./islands/HeroText.tsx";
-import * as $$2 from "./islands/Project.tsx";
-import * as $$3 from "./islands/ProjectList.tsx";
+import * as $0 from './routes/blog/[...slug].tsx';
+import * as $1 from './routes/index.tsx';
+import * as $$0 from './islands/Hero.tsx';
+import * as $$1 from './islands/HeroText.tsx';
+import * as $$2 from './islands/Project.tsx';
+import * as $$3 from './islands/ProjectList.tsx';
 
 const manifest = {
-  routes: {
-    "./routes/blog/[...slug].tsx": $0,
-    "./routes/index.tsx": $1,
-  },
-  islands: {
-    "./islands/Hero.tsx": $$0,
-    "./islands/HeroText.tsx": $$1,
-    "./islands/Project.tsx": $$2,
-    "./islands/ProjectList.tsx": $$3,
-  },
-  baseUrl: import.meta.url,
+    routes: {
+        './routes/blog/[...slug].tsx': $0,
+        './routes/index.tsx': $1,
+    },
+    islands: {
+        './islands/Hero.tsx': $$0,
+        './islands/HeroText.tsx': $$1,
+        './islands/Project.tsx': $$2,
+        './islands/ProjectList.tsx': $$3,
+    },
+    baseUrl: import.meta.url,
 };
 
 export default manifest;
