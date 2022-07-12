f45dbbfe1746896d017cde0a081595153d140a4f
Mads Hougesen <madshougesen@gmail.com>
2022-07-12 14:41:25 +0200

    fix: fixed type errors

--- utils/validDate.ts
+++ utils/validDate.ts
@@ -1,5 +1,3 @@
 export function validDate(unvalidatedDate: string): boolean {
-    // TODO: fix this
-    // @ts-expect-error breaks a lot of data type rules
-    return new Date(unvalidatedDate) !== 'Invalid Date' && !isNaN(new Date(unvalidatedDate));
+    return new Date(unvalidatedDate)?.toString() !== 'Invalid Date' && !isNaN(new Date(unvalidatedDate)?.getTime());
 }
