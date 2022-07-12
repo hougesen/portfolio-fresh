14b2e9c37d3679ac195089166c7031c52b729332
Mads Hougesen <madshougesen@gmail.com>
2022-07-12 13:25:45 +0200

    style: project card hover animation

--- islands/Project.tsx
+++ islands/Project.tsx
@@ -6,7 +6,7 @@ import { IProject } from '../routes/index.tsx';
 export default function Project({ project }: { project: IProject }) {
     return (
         <div
-            class={tw`w-full bg-[#f7f7f7] p-4 flex flex-col border-2 rounded border-solid border-[#101010] min-h-[12rem]`}
+            class={tw`w-full bg-[#f7f7f7] p-4 flex flex-col border-2 rounded border-solid border-[#101010] min-h-[12rem] dark:(border-[#f7f7f7]) project`}
         >
             <div class={tw`flex`}>
                 <a href={project?.url ?? project?.homepageUrl} target='_blank' rel='noreferrer noopener'>
@@ -19,7 +19,7 @@ export default function Project({ project }: { project: IProject }) {
             <div class={tw`flex mt-auto`}>
                 {project.languages.map((language) => (
                     <div
-                        class={tw`text-sm mr-2 rounded-sm px-2 py-1`}
+                        class={tw`text-sm mr-2 rounded-sm px-2 py-1 /* text__flip--reverse */ bg__flip  `}
                         style={{
                             'background-color': language.color,
                         }}
@@ -27,8 +27,11 @@ export default function Project({ project }: { project: IProject }) {
                         <p
                             key={language.name}
                             style={{
-                                color: language.color,
-                                filter: 'brightness(10%)',
+                                color: '#fff',
+                                textShadow: '4px 4px 8px ' + language.color,
+                                //      color: language.color,
+                                //   filter: 'brightness(10%)',
+                                //    filter: 'invert(1) brightness(50%)',
                             }}
                         >
                             {language.name}
